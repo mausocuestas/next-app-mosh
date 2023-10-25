c491b977a9067d9558c9172b98899e5477abe640
Mauro Fuzetto <alcatoobr@yahoo.com.br>
2023-10-25 14:15:49 -0300

    padding 1rem

---
+++ app/component/ProductCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <button onClick={()=> console.log('Clicado')}>Adicione ao Carrinho</button>
+    </div>
+  )
+}
+
+export default ProductCard
