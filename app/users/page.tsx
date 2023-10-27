9be1a240313efe9689e5b479ca37391a35cea477
Mauro Fuzetto <alcatoobr@yahoo.com.br>
2023-10-27 14:31:56 -0300

    styling

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,12 +6,14 @@ interface User {
 }
 
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users'); // vai retornar um objeto
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store' }); // vai retornar um objeto
   const users: User[] = await res.json();
 
   return (
     <>
-      <h1>Users</h1>
+      <h1 className='font-semibold'>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+<br />
       <ul>
         {users.map(user => <li key={user.id}>{user.name}</li>)}
       </ul>
